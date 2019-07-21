7fa5019ae3a4eda4c9ef78f53f9cb3f624b15a7c
Dhanya Himaja <himaja@Dhanyas-MacBook-Pro.local>
2019-07-22 01:47:40 +1000

    Committing Application 4

---
+++ src/ui.js
@@ -0,0 +1,115 @@
+
+class UI {
+    constructor() {
+        this.post = document.querySelector('#posts');
+        this.titleInput = document.querySelector('#title');
+        this.bodyInput = document.querySelector('#body');
+        this.idInput = document.querySelector('#id');
+        this.postSubmit = document.querySelector('.post-submit');
+        this.forState = 'add';
+
+    }
+
+    // show all posts 
+    showPosts(posts) {
+        let output = '';
+         posts.forEach((post) => {
+             output += `
+             <div class="card mb-3">
+             <div class="card-body">
+             <h4 class="card-title">${post.title}</h4>
+             <p class="card-text">${post.body}</p>
+             <a href="#" class="edit card-link" data-id="${post.id}">
+             <i class="fa fa-pencil"></i>
+            </a>
+             <a href="#" class="delete card-link" data-id="${post.id}">
+             <i class="fa fa-remove"></i>
+             </a>
+             </div>
+             </div>
+             `;
+
+         });
+         this.post.innerHTML = output;
+
+    }
+
+    // show alert message
+    showAlert(message, className){
+
+        this.clearAlert();
+        const div = document.createElement('div');
+        div.className = className;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.postsContainer');
+        const posts = document.querySelector('#posts');
+        container.insertBefore(div, posts);
+
+        setTimeout(() => {
+            this.clearAlert();
+        },3000);
+
+    }
+    // clear alert mssage
+    clearAlert(){
+        const currentAlert = document.querySelector('.alert');
+
+        if(currentAlert){
+            currentAlert.remove();
+        }
+
+    }
+
+    // Clear all fields
+    clearFields(){
+        this.titleInput.value='';
+        this.bodyInput.value='';
+    }
+
+    // Fill form to edit
+    fillForm(data) {
+      this.titleInput.value = data.title;
+      this.bodyInput.value= data.body;
+      this.idInput.value= data.id;
+
+      this.changeFormState('edit');
+    }
+
+    // clear ID hidden values
+    clearIDInput(){
+        this.idInput.value = '';
+    }
+    // Change form state 
+    changeFormState(type){
+        if(type === 'edit'){
+        this.postSubmit.textContent ='Update Post';
+        this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+        // Create cancel button
+        const button =  document.createElement('button');
+        button.className = 'post-cancel btn btn-light btn-block';
+        button.appendChild(document.createTextNode('Cancel Edit'));
+
+        // Get Parent
+        const cardForm = document.querySelector('.card-form');
+        // Get element to insert before
+        const formEnd = document.querySelector('.form-end');
+        // Inser cancel button 
+        cardForm.insertBefore(button, formEnd);
+
+        }else{
+            this.postSubmit.textContent ='Post It';
+            this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+            // remove cancel button if present 
+            if(document.querySelector('.post-cancel')){
+                document.querySelector('.post-cancel').remove();
+            }
+            // Clear id from hidden field 
+            this.clearIDInput();
+
+            this.clearFields();
+        }
+    }
+}
+
+export const ui = new UI();
